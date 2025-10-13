7f1dc77bef6cb5d1ebfe040110400358b3555bc7
Mike Darling <mike.darling1@gmail.com>
2025-10-12 22:33:25 -0400

    Updated Screensaver functionality.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import { url } from "inspector";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL('https://api.calendar.emcoded.com/media/img/**')
+    ]
+  }
 };
 
 export default nextConfig;
